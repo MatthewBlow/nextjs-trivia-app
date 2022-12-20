a1c22f425a65023c19c285c1e8fa517ef5dbd82f
Matthew Blow <matthewblow99@gmail.com>
2022-12-20 12:11:27 +0100

    make context cleaner

--- context/mainContext.js
+++ context/mainContext.js
@@ -1,37 +1,33 @@
-import { createContext, useContext, useEffect, useState } from 'react';
-import axios from 'axios';
+import { createContext, useContext, useEffect, useState } from "react";
+import axios from "axios";
 
 const MainContext = createContext();
 
 axios.defaults.baseURL = "https://opentdb.com/";
 
 export const Provider = ({ children }) => {
-  const [response, setResponse] = useState([]);
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(true);
-  const [categories, setCategories] = useState([])
-  const [questions, setQuestions] = useState([])
-  const [url, setUrl] = useState("")
-  
+  const [categories, setCategories] = useState([]);
+  const [questions, setQuestions] = useState([]);
+  const [url, setUrl] = useState("");
+
   axios.defaults.baseURL = "https://opentdb.com/";
 
   useEffect(() => {
-    const fetchQuestions = async() => {
+    const fetchQuestions = async () => {
       try {
-          const response = await axios.get(url);
-          setQuestions(response.data.results)
-          console.log(response.data.results)
-       /*   console.log(response.data.results)
-          console.log(categories.trivia_categories) */
+        const response = await axios.get(url);
+        const data = await response.json();
+        setQuestions(data);
       } catch (error) {
-        console.log(error)
+        console.log(error);
       } finally {
-        setLoading(false)
+        setLoading(false);
       }
-    }
-    fetchQuestions()
-  }, [url])
-  
+    };
+    fetchQuestions();
+  }, [url]);
 
   const mainContextVaues = {
     questions,
@@ -39,10 +35,14 @@ export const Provider = ({ children }) => {
     loading,
     error,
     setUrl,
-    url
-  }
+    url,
+  };
 
-  return <MainContext.Provider value={mainContextVaues}>{children}</MainContext.Provider>
-}
+  return (
+    <MainContext.Provider value={mainContextVaues}>
+      {children}
+    </MainContext.Provider>
+  );
+};
 
 export default MainContext;
